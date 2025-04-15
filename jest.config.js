e9eada7ae1bb289005e5a68cc9e440e54f2ef298
Brian Bennett <brian.bennett2@gmail.com>
2025-04-14 22:34:40 -0400

    work on getting integration tests working

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  global module,
@@ -8,32 +8,29 @@
 /**
  * Provides configuration to Jest
  */
-module.exports = {
 
+module.exports = {
   // set the verbose mode to the value of the DEBUG environment variable
-  verbose: !!process.env.DEBUG || !!process.env.CI,
+  verbose: true,
 
   // Coverage won't be working, since we're not testing JS here
   collectCoverage: false,
 
   globals: {
-
     // the base URL AnchorCMS is running at
-    __BASE_URL__: 'http://localhost',
+    __BASE_URL__: "http://localhost",
 
     // set the DEBUG environment variable
-    __DEBUG__: !!process.env.DEBUG
+    __DEBUG__: !!process.env.DEBUG,
   },
 
-  globalSetup:     './test/setup.js',
-  globalTeardown:  './test/teardown.js',
-  testEnvironment: './test/environment.js',
+  globalSetup: "./test/setup.js",
+  globalTeardown: "./test/teardown.js",
+  testEnvironment: "./test/environment.js",
 
   // do not send notifications if the tests are carried out in a CI environment
-  notify: !process.env.CI,
+  notify: false,
 
   // only match tests within the integration test folder
-  testMatch: [
-    '**/test/integration/**/*.js'
-  ]
+  testMatch: ["**/test/integration/**/*.js"],
 };
--- test/environment.js
+++ test/environment.js
@@ -1,32 +1,31 @@
-'use strict';
+"use strict";
 
 /*
  global module,
  require
  */
 
-const chalk           = require( 'chalk' );
-const NodeEnvironment = require( 'jest-environment-node' );
-const puppeteer       = require( 'puppeteer' );
-const fs              = require( 'fs' );
-const os              = require( 'os' );
-const path            = require( 'path' );
-const jestConfig      = require( '../jest.config' );
+const chalk = require("chalk");
+const NodeEnvironment = require("jest-environment-node").TestEnvironment;
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const jestConfig = require("../jest.config");
 
-const baseDirectory = path.join( os.tmpdir(), 'jest_puppeteer_global_setup' );
+const baseDirectory = path.join(os.tmpdir(), "jest_puppeteer_global_setup");
 
 /**
  * Provides the global environment to puppeteer
  */
 class Environment extends NodeEnvironment {
-
   /**
    * Creates a new environment
    *
    * @param {object} config
    */
-  constructor ( config ) {
-    super( config );
+  constructor(config) {
+    super(config);
   }
 
   /**
@@ -34,18 +33,24 @@ class Environment extends NodeEnvironment {
    *
    * @return {Promise<void>}
    */
-  async setup () {
-    jestConfig.globals.__DEBUG__ && console.log( chalk.yellow( 'Setup Test Environment' ) );
+  async setup() {
+    jestConfig.globals.__DEBUG__ &&
+      console.log(chalk.yellow("Setup Test Environment"));
 
     await super.setup();
 
-    const wsEndpoint = fs.readFileSync( path.join( baseDirectory, 'wsEndpoint' ), 'utf8' );
+    const wsEndpoint = fs.readFileSync(
+      path.join(baseDirectory, "wsEndpoint"),
+      "utf8",
+    );
 
-    if ( !wsEndpoint ) {
-      throw new Error( 'wsEndpoint not found' );
+    if (!wsEndpoint) {
+      throw new Error("wsEndpoint not found");
     }
 
-    this.global.__BROWSER__ = await puppeteer.connect( { browserWSEndpoint: wsEndpoint } );
+    this.global.__BROWSER__ = await puppeteer.connect({
+      browserWSEndpoint: wsEndpoint,
+    });
   }
 
   /**
@@ -53,8 +58,9 @@ class Environment extends NodeEnvironment {
    *
    * @return {Promise<void>}
    */
-  async teardown () {
-    jestConfig.globals.__DEBUG__ && console.log( chalk.yellow( 'Teardown Test Environment' ) );
+  async teardown() {
+    jestConfig.globals.__DEBUG__ &&
+      console.log(chalk.yellow("Teardown Test Environment"));
 
     return await super.teardown();
   }
@@ -65,8 +71,8 @@ class Environment extends NodeEnvironment {
    * @param  {*} script
    * @return {*}
    */
-  runScript ( script ) {
-    return super.runScript( script );
+  runScript(script) {
+    return super.runScript(script);
   }
 }
 
